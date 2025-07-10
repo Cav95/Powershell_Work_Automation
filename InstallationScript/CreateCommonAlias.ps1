f511ac252c914504190b1d53984f615990dbe01f
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-07-10 14:57:08 +0200

    Modify InstallAlias Powershell

--- InstallationScript/CreateCommonAlias.ps1
+++ InstallationScript/CreateCommonAlias.ps1
@@ -1,6 +1,7 @@
-mkdir $PROFILE
+$path="\\SRVUT\Transfert\MATTIA CAVINA\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+mkdir "$PROFILE"
 Set-Location $PROFILE
 Set-Location ../
 Remove-Item ./*.ps1
-Copy-Item "\\SRVUT\Transfert\MATTIA CAVINA\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" $PROFILE
+Copy-Item $path $PROFILE
 #Set-ExecutionPolicy Unrestricted -> to use befor installation in PowerShell amministrator
