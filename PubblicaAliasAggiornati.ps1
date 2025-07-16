4ad3f7059c42fdbfc4f5d60146ce780973bf149a
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-07-16 17:18:05 +0200

    Mdody PubblicaAlias

--- PubblicaAliasAggiornati.ps1
+++ PubblicaAliasAggiornati.ps1
@@ -1,5 +1,5 @@
 $from="C:\0_GitReposItory\Powershell_Work_Automation\AliasPowerShell\*"
 $to="\\SRVUT\Transfert\MATTIA CAVINA\WindowsPowerShell\"
-Remove-Item -Path "$to*" -Recurse -Force
 Copy-Item -Recurse $from $to
+Remove-Item -Path "$to*" -Recurse -Force
 setprofile
