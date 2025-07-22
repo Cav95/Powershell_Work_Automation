74122444b3506bd2612eb4985125a32ecf196161
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-07-22 08:19:43 +0200

    Modify Clear Bak

--- AliasPowerShell/Rimuovi_bak_log.ps1
+++ AliasPowerShell/Rimuovi_bak_log.ps1
@@ -1,4 +1,1 @@
-Get-ChildItem * -Include *.bak -Recurse | Remove-Item
-Get-ChildItem * -Include plot.log -Recurse | Remove-Item
-Get-ChildItem * -Include hardcopy.log -Recurse | Remove-Item
-Get-ChildItem * -Include acad.err -Recurse | Remove-Item
+Get-ChildItem * -Include *.bak,*.log,acad.err -Recurse | Remove-Item
