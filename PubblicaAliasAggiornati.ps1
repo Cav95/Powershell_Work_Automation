52798f316dc34b3c9b3ff883e302c04228fcaad3
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-07-22 08:22:58 +0200

    Correct Publish alias

--- PubblicaAliasAggiornati.ps1
+++ PubblicaAliasAggiornati.ps1
@@ -1,5 +1,4 @@
 $from="C:\0_GitReposItory\Powershell_Work_Automation\AliasPowerShell\*"
 $to="\\SRVUT\Transfert\MATTIA CAVINA\WindowsPowerShell\"
-Copy-Item -Recurse $from $to
 Remove-Item -Path "$to*" -Recurse -Force
-setprofile
+Copy-Item -Recurse $from $to
